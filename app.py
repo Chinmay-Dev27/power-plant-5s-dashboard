9fe47979a3113b4d22939671fee488a93b5a95d8
Chinmay-Dev27 <155307609+Chinmay-Dev27@users.noreply.github.com>
2026-02-18 22:29:11 +0530

    Change font color from black to white in heatmap

--- app.py
+++ app.py
@@ -1768,7 +1768,7 @@ def main():
             
             fig_heat.update_layout(
                 paper_bgcolor='rgba(0,0,0,0)',
-                font_color='black'
+                font_color='white'
             )
             st.plotly_chart(fig_heat, use_container_width=True)
         else:
